ced8b6bcb069381ec0dcf7f50da7e44305098781
niutech <jerzyglowacki@gmail.com>
2024-04-17 17:01:42 +0200

    Add Spectre.css

--- node/index.js
+++ node/index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = 8080;
 
 app.get("/", (req, res, next) => {
     const indexFile = "../static/index.html";
